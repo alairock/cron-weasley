601c42c2aadd9b5659a192c6707512df03aeb13b
Skyler Lewis <skyler.lewis@canopytax.com>
2018-03-30 15:23:21 -0600

    rename

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-__version__ = '1.0.13'
+__version__ = '1.0.1'
 
-setuptools.setup(name='cron-weasley',
+setuptools.setup(name='cronweasley',
                  version=__version__,
                  description='Cronjobs for Wizards',
                  long_description=open('README').read().strip(),
                  author='alairock',
                  author_email='sblnog@gmail.com',
                  url='http://github.com/alairock/cron-weasley',
-                 py_modules=['cron-weasley'],
+                 py_modules=['cronweasley'],
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
