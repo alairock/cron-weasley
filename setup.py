0f2ed3eab3d84e61b3808123aa9d03fdbdbf0069
Skyler Lewis <skyler.lewis@canopytax.com>
2018-04-02 10:37:05 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-import setuptools
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-__version__ = '1.0.2'
+here = path.abspath(path.dirname(__file__))
 
-setuptools.setup(name='cronweasley',
-                 version=__version__,
-                 description='Cronjobs for Wizards',
-                 long_description=open('README').read().strip(),
-                 author='alairock',
-                 author_email='sblnog@gmail.com',
-                 url='http://github.com/alairock/cron-weasley',
-                 py_modules=['cronweasley'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='cron crontab cronjob',
-                 classifiers=['Development Status :: 4 - Beta'])
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cronweasley',
+    version='1.1.0',
+    description='Cronjobs for Wizards',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/alairock/cron-weasley',
+    author='alairock',
+    author_email='sblnog@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='cron crontab cronjob',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[],
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    }
+)
